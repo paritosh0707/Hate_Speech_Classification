7f94c3cfab2ee5f73067d55c41169bf827f28abf
Paritosh Sharma <paritoshsharma0707@gmail.com>
2024-02-24 20:43:47 +0530

    implemented data transformation component

--- src/Hate_Speech_Classification/entity/config_entity.py
+++ src/Hate_Speech_Classification/entity/config_entity.py
@@ -3,14 +3,25 @@ from Hate_Speech_Classification.constants import *
 from Hate_Speech_Classification.logger import logging
 import os
 
-# @dataclass(frozen=True)
+@dataclass
 class DataIngestionConfig:
     def __init__(self):
         self.BUCKET_NAME :str = BUCKET_NAME
         self.BUCKET_FOLDER_NAME :str = BUCKET_FOLDER_NAME
         self.DATA_INGESTION_ARTIFACTS_DIR: str = os.path.join(os.getcwd(),ARTIFACTS_DIR,DATA_INGESTION_ARTIFACTS_DIR)
-        self.RAW_DATA_FILE_PATH: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_IMBALANCE_DATA_DIR)
-        self.IMBALANCED_DATA_FILE_PATH: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_RAW_DATA_DIR)
-        # self.ZIP_FILE_DIR = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR)
-        # self.ZIP_FILE_PATH = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,self.ZIP_FILE_NAME)
-        
+        self.RAW_DATA_FILE_PATH: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_RAW_DATA_DIR)
+        self.IMBALANCED_DATA_FILE_PATH: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_IMBALANCE_DATA_DIR)
+
+
+@dataclass
+class DataTransformationConfig:
+    def __init__(self):
+        self.DATA_TRANSFORMATION_ARTIFACTS_DIR: str = os.path.join(os.getcwd(),ARTIFACTS_DIR,DATA_TRANSFORMATION_ARTIFACTS_DIR)
+        self.TRANSFORMED_FILE_PATH = os.path.join(self.DATA_TRANSFORMATION_ARTIFACTS_DIR,TRANSFORMED_FILE_NAME)
+        self.ID = ID
+        self.AXIS = AXIS
+        self.INPLACE = INPLACE 
+        self.DROP_COLUMNS = DROP_COLUMNS
+        self.CLASS = CLASS 
+        self.LABEL = LABEL
+        self.TWEET = TWEET
--- src/Hate_Speech_Classification/pipeline/train_pipeline.py
+++ src/Hate_Speech_Classification/pipeline/train_pipeline.py
@@ -1,17 +1,19 @@
 import sys
 import os
 from Hate_Speech_Classification.components.data_ingestion import DataIngestion
+from Hate_Speech_Classification.components.data_transforamation import DataTransformation
 from Hate_Speech_Classification.logger import logging
 from Hate_Speech_Classification.exception import CustomException
-from Hate_Speech_Classification.entity.config_entity import DataIngestionConfig
-from Hate_Speech_Classification.entity.artifact_entity import DataIngestionArtifacts
+from Hate_Speech_Classification.entity.config_entity import DataIngestionConfig, DataTransformationConfig
+from Hate_Speech_Classification.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts
 
 
 class TrainingPipeline:
     def __init__(self) -> None:
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_transformation_config = DataTransformationConfig()
 
-    def initiate_data_ingestion(self)-> DataIngestionArtifacts:
+    def start_data_ingestion(self)-> DataIngestionArtifacts:
         logging.info("Entered initiate_data_ingestion method of TrainingPipeline class")
         try:
             data_ingestion = DataIngestion(
@@ -24,13 +26,30 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e,sys) from e
         
+    def start_data_transformation(self, data_ingestion_artifacts = DataIngestionArtifacts) -> DataTransformationArtifacts:
+        logging.info("Entered the start_data_transformation method of TrainPipeline class")
+        try:
+            data_transformation = DataTransformation(
+                data_ingestion_artifacts = data_ingestion_artifacts,
+                data_transformation_config=self.data_transformation_config
+            )
+
+            data_transformation_artifacts = data_transformation.initiate_data_transformation()
+            
+            logging.info("Exited the start_data_transformation method of TrainPipeline class")
+            return data_transformation_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
 
     def run_pipeline(self)->None:
         logging.info("Entering run_pipeline method of TrainingPipeline class")
         try:
-            data_ingestion_artifacts = self.initiate_data_ingestion()
-            print(data_ingestion_artifacts.imbalance_data_file_path)
-            print(data_ingestion_artifacts.raw_data_file_path)
+            data_ingestion_artifacts = self.start_data_ingestion()
+            data_transformation_artifacts = self.start_data_transformation(
+                data_ingestion_artifacts=data_ingestion_artifacts
+            )
         except Exception as e:
             raise CustomException(e,sys) from e
         
